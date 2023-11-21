a1ee8efae18d8ccb896f40d0d1e57670ca616029
kevin-sotomayor <kevin.sotomayor@outlook.fr>
2023-11-21 20:14:02 +0100

    form is a bit more sophisticated

--- app/routes/admin._index.tsx
+++ app/routes/admin._index.tsx
@@ -1,6 +1,15 @@
-import type { LinksFunction } from "@remix-run/node"
+import type { LinksFunction, ActionFunctionArgs } from "@remix-run/node"
+import { Form } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { PrismaClient } from "@prisma/client";
+
 
 import admin from "../styles/admin.css";
+
+import status from "../json/status.json";
+
+
+const prisma = new PrismaClient();
 
 
 export const links: LinksFunction = () => {
@@ -9,15 +18,32 @@ export const links: LinksFunction = () => {
   ]
 }
 
+// TODO: type
+export async function action({ request }: { request: any }) {
+  const body = await request.formData();
+  const data = Object.fromEntries(body.entries());
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email
+    }
+  });
+  if (!user) {
+    return json(status.error.login_failure);
+  }
+  return user;
+}
+
+// TODO: text content in JSON file
 export default function Admin() {
+  // const user = useLoaderData();
   return (
     <main className="admin-page">
       <h2 className="admin-page__title">Admin</h2>
-      <form className="admin-page__form" action="">
-        <input className="admin-page__input" type="email" placeholder="Adresse mail" autoComplete="email"/>
-        <input className="admin-page__input" type="password" placeholder="Mot de passe" autoComplete="current_password"/>
-        <button className="admin-page__button">Se connecter</button>
-      </form>
+      <Form className="admin-page__form" method="post" preventScrollReset>
+        <input className="admin-page__input" name="email" type="email" placeholder="Adresse mail" autoComplete="" required/>
+        <input className="admin-page__input" name="password" type="password" placeholder="Mot de passe" autoComplete="current_password" required/>
+        <button className="admin-page__button" type="submit">Se connecter</button>
+      </Form>
     </main>
   )
 }
