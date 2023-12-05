1c16f034a22ac05cf91cca6bb63e5761d61bc150
kevin-sotomayor <kevin.sotomayor@outlook.fr>
2023-12-05 16:51:45 +0100

    added a route to dashboard for pages

--- app/routes/admin.dashboard.pages.tsx
+++ app/routes/admin.dashboard.pages.tsx
@@ -1,5 +1,25 @@
+import { Outlet, Link } from "@remix-run/react";
+
+import dashboard from "../json/dashboard.json";
+
+
 export default function AdminDashboardPages() {
+  const section: any = dashboard.sections[1];
+  console.log(section);
   return (
-    <p>Pages</p>
+    <section className="dashboard-page__subsections">
+      <nav className="subsections__nav">
+        <ul className="subsections__list">
+          {
+            section?.subsections.map((subsection: any, index: number) => (
+              <li key={index} className="subsections__list-element">
+                <Link to={`${subsection.url}`} className="subsections__list-link">{subsection.name}</Link>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+      <Outlet/>
+    </section>
   )
 }
